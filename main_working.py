4ddb63d259ed486d01899531d49e4d1495404351
Paul Mason <pdm126@hotmail.co.uk>
2015-07-02 18:00:55 +0100

    Create main_working.py
    First comit working file, may be broken. Tests two methods of feed burner. One brings in feeds as their own dicts (?), one reads in a list of urls which seems to make a big dict where some entries are lists.
    
    reading/stealing from:
    
    https://docs.python.org/2/library/tdtypes.html
    https://docs.python.org/2/tutorial/introduction.html#lists
    https://iamrafiul.wordpress.com/2013/04/25/rss-feed-parsing-in-python-using-feedparser-a-quick-yet-complete-tutorial/
    https://pythonhosted.org/feedparser/uncommon-rss.html
    http://stackoverflow.com/questions/28683619/how-can-i-parse-multiple-urls-in-feedparser-python

---
+++ main_working.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python2.7                                                                                                                              
+
+import feedparser
+
+# deals with where all the feeds come in                                                                                                              
+#feed = feedparser.parse('http://www.reddit.com/r/python/.rss')                                                                                       
+#feed = feedparser.parse('http://feeds.bbci.co.uk/news/scotland/rss.xml?edition=uk')                                                                  
+
+# multiple feed test                                                                                                                                  
+FEED_List = [
+    'http://feeds.bbci.co.uk/news/scotland/rss.xml?edition=uk',
+    'http://www.gameinformer.com/b/MainFeed.aspx?Tags=preview',
+    ]
+
+feeds = []
+for url in FEED_List:
+    feeds.append(feedparser.parse(url))
+
+for feed in feeds:
+    for post in feed.entries:
+        print len(post)
+
+# deals with kewords                                                                                                                                  
+
+keyword = 'hacked'
+
+# parses and processes feeds                                                                                                                          
+
+title = feed['entries'][1].title,
+description =  feed['entries'][1].summary,
+url = feed['entries'][1].link,
+posts = []
+for i in range(0,len(feed['entries'])):
+    posts.append({
+        'title': feed['entries'][i].title,
+        'description': feed['entries'][i].summary,
+        'url': feed['entries'][i].link,
+    })
+
+# searches feeds for keywords                                                                                                                         
+
+
+
+# displays results                                                                                                                                    
+print title
+print description
+print url
+print 'Feed Details'
+print '****'
+print 'Available Feeds'
+print feed['feed']['link']
+print '****'
+print 'Number of Entries'
+print len(posts)
+print 'Most Recent Entry'
+print feed['entries'][0]['title']
+print feed.entries[0]['link']
+print feed
+
